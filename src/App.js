1454a7ed4c5c839bc3c0040e731117ed3344086c
ShivR13 <37413974+ShivR13@users.noreply.github.com>
2021-02-15 16:32:49 +0530

    second version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import "./styles2.css";
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import Note from "../src/components/Note";
+import CreateArea from "../src/components/CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <h1>Hi I am Shiv</h1>
-      <h2>!st hostedraect app</h2>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
